a2457efc91c55f036dc8d37ac568e7eecff61857
watson <watson.chen@ambernet.com.tw>
2020-09-18 16:24:16 +0800

    create one google map component

---
+++ src/components/Gmap.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="google-map" id="map"></div>
+</template>
+
+<script>
+export default {
+  // 接收來自父元件的地圖設定資訊
+  name: 'Gmap',
+  props: {
+    center: {
+      type: Object,
+      default: () => ({ lat: 25.0325917, lng: 121.5624999 })
+    },
+    zoom: {
+      type: Number,
+      default: 14
+    },
+    streetViewControl: {
+      type: Boolean,
+      default: true
+    },
+    mapTypeControl: {
+      type: Boolean,
+      default: true
+    },
+    fullscreenControl: {
+      type: Boolean,
+      default: true
+    },
+    zoomControl: {
+      type: Boolean,
+      default: true
+    },
+    restaurants: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      map: null,
+      infowindow: null,
+      markers: []
+    }
+  },
+  // 當發現中心位置改變時，更新中心位置與地標
+  watch: {
+    center (val) {
+      this.resetCenter()
+      this.setMarker()
+    }
+  },
+  mounted () {
+    this.initMap()
+    this.setMarker()
+  },
+  methods: {
+    initMap () {
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: this.center,
+        zoom: this.zoom,
+        maxZoom: 20,
+        minZoom: 3,
+        streetViewControl: this.streetViewControl,
+        mapTypeControl: this.mapTypeControl,
+        fullscreenControl: this.fullscreenControl,
+        zoomControl: this.zoomControl
+      })
+    },
+    resetCenter () {
+      // set center
+      this.map.panTo({ lat: this.center.lat, lng: this.center.lng })
+    },
+    clearMarkers () {
+      this.markers.forEach(marker => marker.setMap(null))
+      this.markers = []
+    },
+    setMarker () {
+      // clear existing markers
+      this.clearMarkers()
+      this.restaurants.forEach(location => {
+        const marker = new google.maps.Marker({
+          position: { lat: location.lat, lng: location.lng },
+          map: this.map
+        })
+        // save markers
+        this.markers.push(marker)
+        const infowindow = new google.maps.InfoWindow({
+          content: `
+          <div id="content">
+            <p id="firstHeading" class="firstHeading">${location.name}</p>
+          </div>
+        `,
+          maxWidth: 200
+        })
+        marker.addListener('click', () => {
+          if (this.infowindow) this.infowindow.close()
+          infowindow.open(this.map, marker)
+          this.infowindow = infowindow
+        })
+      })
+    }
+  }
+}
+</script>
